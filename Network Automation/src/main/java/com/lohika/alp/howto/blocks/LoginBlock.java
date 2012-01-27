fe59b148209344c6b36bf4029f181996cdd34795
Anton Smorodsky <antons@localhost.localdomain>
2012-01-27 11:41:27 +0200

    first commit

---
+++ Network Automation/src/main/java/com/lohika/alp/howto/blocks/LoginBlock.java
@@ -0,0 +1,29 @@
+package com.lohika.alp.howto.blocks;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import com.lohika.alp.selenium.log.LogDescription;
+import com.lohika.alp.utils.Environment;
+
+public class LoginBlock {
+
+
+	@LogDescription(name = "login_name", type = "edit")
+	@FindBy(name="login_name")
+	private WebElement login_name;
+    
+	@LogDescription(name = "User's password", type = "edit")
+	@FindBy (name = "login_password")
+	private WebElement login_pass;
+	
+	@LogDescription (name = "Login Button", type = "button")
+	@FindBy (name = "button")
+	private WebElement login_btn_label;
+	
+	   public void madeLogin() {
+		   login_name.sendKeys(Environment.getUsername());
+		   login_pass.sendKeys(Environment.getPass());
+		   login_btn_label.click();
+	   }
+	}
+
---
+++ Network Automation/src/main/java/com/lohika/alp/howto/tests/LoginPositive.java
@@ -0,0 +1,49 @@
+package com.lohika.alp.howto.tests;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.lohika.alp.selenium.AlpPageFactory;
+import com.lohika.alp.selenium.AlpWebDriverFactory;
+import com.lohika.alp.utils.Environment;
+
+public class LoginPositive {
+
+	private WebDriver drv;
+
+	private LoginBlock loginblock;
+
+	
+
+	@BeforeClass(alwaysRun = true)
+	public void initClass() throws MalformedURLException {
+
+		DesiredCapabilities cap = DesiredCapabilities.firefox();
+
+		cap.setCapability(CapabilityType.TAKES_SCREENSHOT, true);
+
+		drv = AlpWebDriverFactory.getDriver(Environment.getServerURL(), cap);
+
+		drv.get(Environment.getAUTLink());
+
+		loginblock = AlpPageFactory.initElements(drv, LoginBlock.class);
+
+
+		
+	}
+
+	@Test(groups = "test")
+	public void testLogin() throws InterruptedException {
+		
+		loginblock.madeLogin();
+
+
+		
+		
+	}
+}
